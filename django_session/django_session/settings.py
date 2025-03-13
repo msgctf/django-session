69809dd9df2fa53ac37bf2fc3babe6d81303e17e
Jongcoding <ialleejy@gmail.com>
2025-03-14 04:47:36 +0900

    setting: django session cookie

--- django_session/django_session/settings.py
+++ django_session/django_session/settings.py
@@ -16,6 +16,9 @@ from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
+
 env_path = os.path.join(BASE_DIR, '.env')
 load_dotenv(env_path)
 
